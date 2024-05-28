a73ae16de061bb93f64115d2dcad677b44d7f486
benzy78 <westvillage815@gmail.com>
2024-05-28 23:16:15 +0900

    dockerの環境設定

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_29_083107) do
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
