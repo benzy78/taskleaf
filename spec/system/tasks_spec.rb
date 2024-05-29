242260492631aae7c05f81f836787994358d0d18
benzy78 <westvillage815@gmail.com>
2024-05-29 23:47:26 +0900

    詳細表示機能のspecを追加

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,23 +1,21 @@
 require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
+
+  # ユーザーA、Bをletで作成
+  let(:user_a){FactoryBot.create(:admin_user, name: 'ユーザーA', email: 'a@example.com')}
+  let(:user_b){FactoryBot.create(:admin_user, name: 'ユーザーB', email: 'b@example.com')}
+  # ユーザーAのタスクをletで作成
+  let!(:task_a){FactoryBot.create(:task, name: '最初のタスク', user: user_a)}
+
+  before do
+    # ユーザーXでログインする（共通処理）
+    visit login_path
+    fill_in 'メールアドレス', with: login_user.email
+    fill_in 'パスワード', with: login_user.password
+    click_button 'ログインする'
+  end
   describe '一覧表示機能' do
-
-    # ユーザーA、Bをletで作成
-    let(:user_a){FactoryBot.create(:admin_user, name: 'ユーザーA', email: 'a@example.com')}
-    let(:user_b){FactoryBot.create(:admin_user, name: 'ユーザーB', email: 'b@example.com')}
-
-    before do
-      # ユーザーAのタスク作成
-      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
-
-      # ユーザーXでログインする（共通処理）
-      visit login_path
-      fill_in 'メールアドレス', with: login_user.email
-      fill_in 'パスワード', with: login_user.password
-      click_button 'ログインする'
-    end
-
     context 'ユーザーAがログインしているとき' do
       # login_userの定義
       let(:login_user) {user_a}
@@ -35,6 +33,20 @@ describe 'タスク管理機能', type: :system do
       it 'ユーザーAのタスクが表示されない' do
         #ユーザーAのタスクが表示されないことを確認する
         expect(page).to have_no_content '最初のタスク'
+      end
+    end
+  end
+
+  describe '詳細表示機能' do
+    context 'ユーザーAがログインしているとき' do
+      let(:login_user) {user_a}
+
+      before do
+        visit task_path(task_a)
+      end
+
+      it 'ユーザーAが作成したタスクが表示される' do
+        expect(page).to have_content '最初のタスク'
       end
     end
   end
